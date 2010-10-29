d8b09e8fb3846d7c02a8e8dc4dd9b9f7aebfe8a7
Jonny Lamb <jonny.lamb@collabora.co.uk>
2010-10-29 12:35:39 +0100

    configure: use automake 1.11
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- autogen.sh
+++ autogen.sh
@@ -3,13 +3,13 @@ set -e
 
 if test -n "$AUTOMAKE"; then
     : # don't override an explicit user request
-elif automake-1.9 --version >/dev/null 2>/dev/null && \
-     aclocal-1.9 --version >/dev/null 2>/dev/null; then
-    # If we have automake-1.9, use it. This helps to ensure that our build
-    # system doesn't accidentally grow automake-1.10 dependencies.
-    AUTOMAKE=automake-1.9
+elif automake-1.11 --version >/dev/null 2>/dev/null && \
+     aclocal-1.11 --version >/dev/null 2>/dev/null; then
+    # If we have automake-1.11, use it. This helps to ensure that our build
+    # system doesn't accidentally grow other dependencies.
+    AUTOMAKE=automake-1.11
     export AUTOMAKE
-    ACLOCAL=aclocal-1.9
+    ACLOCAL=aclocal-1.11
     export ACLOCAL
 fi
 
